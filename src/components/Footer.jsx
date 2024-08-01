55db3c35805e5151cf54ebc98f4b935f21c4a254
Aayush Bakre <29601578+WebHead2211@users.noreply.github.com>
2024-08-02 01:22:50 +0530

    Add link to repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return <footer>
-    <h1><a href="https://github.com/WebHead2211/quote-generator/">Aayush Bakre &bull; 2024</a></h1>
+    <h1><a href="https://github.com/WebHead2211/quote-generator/" target="_blank">Aayush Bakre &bull; 2024</a></h1>
   </footer>;
 }
